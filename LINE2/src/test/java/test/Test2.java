2429bae4801e2f282377b24095a8d49f3e9fcb30
AvinashReddy211 <avinashnukala1999@gmail.com>
2025-02-25 21:29:07 +0530

    My Commit

---
+++ LINE2/src/test/java/test/Test2.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test2 {
+
+	@Test
+	public void test() {
+	System.out.println("Test1");
+	}
+
+}
---
+++ LINE2/src/test/java/test/Test5.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test5 {
+
+	@Test
+	public void test() {
+	System.out.println("Test1");
+	}
+
+}
